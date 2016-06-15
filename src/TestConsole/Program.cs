6c364009d4ec0da26688d04add4008cac6458d7f
Justin Emgarten <justin@emgarten.com>
2016-06-15 11:15:44 -0700

    initial refactoring

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace NuGet.CatalogVisitor
+namespace TestConsole
 {
-    public static class Program
+    class Program
     {
         static void Main()
         {
